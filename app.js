44d30740e7c22d43d85d895e32f1f6850863f669
Sanath <sanaabdulmanaf88@gmail.com>
2022-12-08 11:29:10 +0530

    model schema created for user

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const mongoose=require("mongoose");
 
 const app=express();
 
+mongoose.set('strictQuery',true);  //for depressing the warning shown in the console
+
 mongoose.connect("mongodb+srv://admin:kqAwhQkuV6qPIMAh@cluster0.9fzmxm6.mongodb.net/?retryWrites=true&w=majority")
 .then(()=>app.listen(5000,()=>
 console.log("Connected nd listening to the port 5000")
